6e05a609eb3878d749b29a1e0e5d0d3751cc70b0
AntonFet <48539443+AntonFet@users.noreply.github.com>
2024-05-26 14:25:03 +0700

    My calculations

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        try {
+            FileReader fileReader = new FileReader("src/access.log");
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+
+            int totalLines = 0;
+            int maxLength = 0;
+            int minLength = Integer.MAX_VALUE;
+
+            while ((line = reader.readLine()) != null) {
+                int length = line.length();
+                totalLines++;
+                if (length > maxLength) {
+                    maxLength = length;
+                }
+                if (length < minLength) {
+                    minLength = length;
+                }
+
+                if (length > 1024) {
+                    throw new LineTooLongException("Line longer than 1024 characters encountered");
+                }
+            }
+
+            System.out.println("Общее количество строк в файле: " + totalLines);
+            System.out.println("Длина самой длинной строки в файле: " + maxLength);
+            System.out.println("Длина самой короткой строки в файле: " + minLength);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         }
     }
